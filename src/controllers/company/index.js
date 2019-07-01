f22399fc35a182eda30208fe7611ce45473c6fbf
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 11:19:49 -0300

    controllers: add company

---
+++ src/controllers/company/index.js
@@ -0,0 +1,26 @@
+const database = require('../../database')
+const CompanyModel = database.model('company')
+const { defaultResponse, errorResponse } = require('../../utils')
+
+class Company {
+
+  async create(companyData) {
+    try {
+      const companyResponse = await CompanyModel.create(companyData)
+      return defaultResponse(companyResponse, 201)
+    } catch (error) {
+      return errorResponse(error, 422)
+    }
+  }
+
+  async getByid(id) {
+    try {
+      const response = await CompanyModel.findByPk(id)
+      return defaultResponse(response)
+    } catch (error) {
+      return errorResponse(error)
+    }
+  }
+}
+
+module.exports = Company
