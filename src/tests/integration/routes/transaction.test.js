5d33d91aed7e2f80d669b72bb87fb6320ec0e99f
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 20:39:03 -0300

    chore: fix test of transactions

--- src/tests/integration/routes/transaction.test.js
+++ src/tests/integration/routes/transaction.test.js
@@ -1,11 +1,15 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-
+const { getFinalNumbersCard } = require('../../../utils/transactionSpec')
 const app = require('../../../app')
-const url = '/api/v1/transactions'
 
 const request = chai.use(chaiHttp).request(app)
 const requester = request.keepOpen()
+
+const companyMock = {
+  social_name: 'Company Name LTDA',
+  cnpj: '11222333000100',
+}
 
 const transactionMock = {
   amount: 1000,
@@ -13,18 +17,28 @@ const transactionMock = {
   card_cvv: '677',
   card_expiration_date: '1220',
   card_holder_name: 'Alexandre dos Santos Soares',
-  description: 'Smart buy now',
+  description: 'PS4 Deluxe Pro',
+  payment_method: 'debt_card',
+  payment_date: 'Fri Jun 28 2019 15:43:37 GMT-0300 (Brasilia Standard Time)',
 }
 
 describe('Routes: Transaction', () => {
-  describe('Route POST /transactions', () => {
+
+  let url = null
+
+  beforeAll(async () => {
+    const { body } = await requester.post('/api/v1/companies').send(companyMock)
+    url = `/api/v1/companies/${body.id}/transactions`
+  })
+
+  describe('Route POST /companies/:companyId/transactions', () => {
     test('should create a transaction', async () => {
       const { status, body } = await requester.post(url).send(transactionMock)
       
       expect(body.id).toBeTruthy()
       expect(body.amount).toBe(transactionMock.amount)
       expect(body.card_expiration_date).toBe(transactionMock.card_expiration_date)
-      expect(body.card_number).toBe(transactionMock.card_number)
+      expect(body.card_number).toBe(getFinalNumbersCard(transactionMock.card_number))
       expect(body.card_cvv).toBe(transactionMock.card_cvv)
       expect(body.card_expiration_date).toBe(transactionMock.card_expiration_date)
       expect(body.card_holder_name).toBe(transactionMock.card_holder_name)
@@ -39,34 +53,22 @@ describe('Routes: Transaction', () => {
 
   })
   
-  describe('Route GET /transactions', () => {
+  describe('Route GET /companies/:companyId/transactions', () => {
     test('should return a list of transactions', async () => {
+      
+      await requester.post(url).send(transactionMock)
+      await requester.post(url).send(transactionMock)
+      await requester.post(url).send(transactionMock)
+
       const { status, body } = await requester.get(url)
       expect(body.length > 0).toBeTruthy()
       expect(status).toBe(200)
     })
-  })
-
-  describe('Route GET /transactions/:id', () => {
-    test('should return a transaction', async () => {
-      const { body: created } = await requester.post(url).send(transactionMock)
-      const { status, body } = await requester.get(`${url}/${created.id}`)
-      
-      expect(body.id).toBe(created.id)
-      expect(body.amount).toBe(created.amount)
-      expect(body.card_expiration_date).toBe(created.card_expiration_date)
-      expect(body.card_number).toBe(created.card_number)
-      expect(body.card_cvv).toBe(created.card_cvv)
-      expect(body.card_expiration_date).toBe(created.card_expiration_date)
-      expect(body.card_holder_name).toBe(created.card_holder_name)
-      expect(status).toBe(200)
-    })
-
-    test('should return a error id null', async () => {
-      const { status, body } = await requester.get(`${url}/null`)
+    
+    test('should return a error companyId null', async () => {
+      const { status, body } = await requester.get('/api/v1/companies/null/transactions')
       expect(body.error).toBeTruthy()
       expect(status).toBe(400)
     })
   })
-
 })
--- src/tests/unit/controllers/transaction/index.test.js
+++ src/tests/unit/controllers/transaction/index.test.js
@@ -1,4 +1,11 @@
 const TransationController = require('../../../../controllers/transaction')
+const CompanyController = require('../../../../controllers/company')
+const { getFinalNumbersCard } = require('../../../../utils/transactionSpec')
+
+const companyMock = {
+  social_name: 'Company Name LTDA',
+  cnpj: '11222333000100',
+}
 
 const transactionMock = {
   amount: 1000,
@@ -6,48 +13,42 @@ const transactionMock = {
   card_cvv: '677',
   card_expiration_date: '1220',
   card_holder_name: 'Alexandre dos Santos Soares',
-  description: 'Smart buy now',
+  description: 'PS4 Deluxe Pro',
+  payment_method: 'debt_card',
+  payment_date: 'Fri Jun 28 2019 15:43:37 GMT-0300 (Brasilia Standard Time)',
 }
 
 describe('Controllers: Transaction', () => {
 
   let transationController = null
-  beforeEach(() => {
+  let companyId = null
+
+  beforeAll(async () => {
     transationController = new TransationController()
+    const companyController = new CompanyController()
+    
+    const { data: { id }} = await companyController.create(companyMock)
+    companyId = id
   })
   
   describe('Get all transactions: getAll()', () => {
     test('should return a list of transactions', async () => {
-      await transationController.create(transactionMock)
-      await transationController.create(transactionMock)
+      await transationController.create(transactionMock, companyId)
+      await transationController.create(transactionMock, companyId)
       
-      const { data } = await transationController.getAll()
+      const { data } = await transationController.getAll(companyId)
       expect(data.length > 0).toBeTruthy()
     });
   })
 
-  describe('Get a transaction: getById()', () => {
-    test('should return a transaction', async() => {
-      const { data } = await transationController.create(transactionMock)
-      const { data: response } = await transationController.getByid(data.id)
-      expect(response.id).toBe(data.id)
-      expect(response.amount).toBe(data.amount)
-      expect(response.card_expiration_date).toBe(data.card_expiration_date)
-      expect(response.card_number).toBe(data.card_number)
-      expect(response.card_cvv).toBe(data.card_cvv)
-      expect(response.card_expiration_date).toBe(data.card_expiration_date)
-      expect(response.card_holder_name).toBe(data.card_holder_name)
-    })
-  })
-
   describe('Create a transaction: create()', () => {
     test('should create a transactions', async () => {
-      const { data } = await transationController.create(transactionMock)
+      const { data } = await transationController.create(transactionMock, companyId)
 
       expect(data.id).toBeTruthy()
       expect(data.amount).toBe(transactionMock.amount)
       expect(data.card_expiration_date).toBe(transactionMock.card_expiration_date)
-      expect(data.card_number).toBe(transactionMock.card_number)
+      expect(data.card_number).toBe(getFinalNumbersCard(transactionMock.card_number))
       expect(data.card_cvv).toBe(transactionMock.card_cvv)
       expect(data.card_expiration_date).toBe(transactionMock.card_expiration_date)
       expect(data.card_holder_name).toBe(transactionMock.card_holder_name)
