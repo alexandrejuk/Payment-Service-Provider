09e845dbb7f3cd15f0702ffb10aa077bfe0f2ea0
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 11:21:53 -0300

    controllers: create transaction with payable

--- src/controllers/transaction/index.js
+++ src/controllers/transaction/index.js
@@ -1,6 +1,9 @@
 const database = require('../../database')
 const TransactionModel = database.model('transaction')
+const PayableModel = database.model('payable')
+
 const { defaultResponse, errorResponse } = require('../../utils')
+const { transactionSpec, payableSpec } = require('../../utils/transactionSpec')
 
 class Transaction {
   async getAll() {
@@ -11,12 +14,19 @@ class Transaction {
       return errorResponse(error)
     }
   }
-
-  async create(transaction) {
+  
+  async create(transactionData) {
+    const transaction = await database.transaction()
     try {
-      const response = await TransactionModel.create(transaction)
-      return defaultResponse(response, 201)
+      const transactionResponse = await TransactionModel.create(
+        transactionSpec(transactionData), { transaction }
+      )
+      
+      await PayableModel.create(payableSpec(transactionResponse), { transaction })
+      await transaction.commit()
+      return defaultResponse(transactionResponse, 201)
     } catch (error) {
+      await transaction.rollback()
       return errorResponse(error, 422)
     }
   }
