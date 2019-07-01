0c831655cc7c810e57607650196a43eead5aa27c
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 11:20:03 -0300

    routes: add company

---
+++ src/routes/company.js
@@ -0,0 +1,17 @@
+const CompanyController = require('../controllers/company')
+
+module.exports = (app) => {
+  const companyController = new CompanyController()
+
+  app.route('/api/v1/companies')
+    .post(async (req, res) => {
+      const { data, statusCode } = await companyController.create(req.body)
+      res.status(statusCode).json(data)
+    })
+
+  app.route('/api/v1/companies/:id')
+    .get(async (req, res) => {
+      const { data, statusCode } = await companyController.getByid(req.params.id)
+      res.status(statusCode).json(data)
+    })
+}
