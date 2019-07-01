06e84d7ff1c15a6a46687af4774dabeac58c9791
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 16:42:34 -0300

    utils: add transactions spec to normalize data

--- src/utils/transactionSpec.js
+++ src/utils/transactionSpec.js
@@ -1,5 +1,8 @@
 const moment = require('moment')
+
 const credit_card = 'credit_card'
+const salesTaxCredit = .05
+const salesTaxDebt = .03
 
 const getFinalNumbersCard = value => value.slice(12)
 
@@ -21,11 +24,20 @@ const transactionSpec = transactionData => {
   })
 }
 
+const salesTax = (type, total) => {
+  switch (type) {
+    case credit_card:
+      return (total - (total*salesTaxCredit))
+    default:
+      return (total - (total*salesTaxDebt))
+  }
+}
+
 const payableSpec = transactionData => {
   const { id, amount, payment_method, payment_date } = transactionData
   return ({
     transactionId: id,
-    amount: amount,
+    amount: salesTax(payment_method, amount),
     status: setStatusPayment(payment_method),
     payment_date: setPaymentDate(payment_method, payment_date)
   })
