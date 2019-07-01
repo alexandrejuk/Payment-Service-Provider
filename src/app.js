c86d38feaf5b9741181f14e7e2579e2f27a00b97
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 16:44:01 -0300

    routes: add new import of route

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const Express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const balanceRouter = require('./routes/balance')
 const companyRouter = require('./routes/company')
 const transactionRouter = require('./routes/transaction')
 
 const app = Express()
 
-app.use(bodyParser.json())
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+balanceRouter(app)
 companyRouter(app)
 transactionRouter(app)
 
