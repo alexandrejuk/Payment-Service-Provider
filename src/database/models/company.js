624a15434dbf4fad0c6865803069196257daf84d
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 11:16:22 -0300

    database: add company model

---
+++ src/database/models/company.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+
+const Company = (sequelize) => {
+  const Company = sequelize.define('company', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    social_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    cnpj: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    }
+  })
+
+  return Company
+}
+
+module.exports = Company
