79aca82d313d9f90e354a9adcc2ffc9ff7a944ff
Alexandre S. Soares <alexandre.s.soares@accenture.com>
2019-07-01 16:52:57 -0300

    controllers: add companyId to transaction

--- src/controllers/transaction/index.js
+++ src/controllers/transaction/index.js
@@ -1,42 +1,39 @@
 const database = require('../../database')
 const TransactionModel = database.model('transaction')
 const PayableModel = database.model('payable')
-
 const { defaultResponse, errorResponse } = require('../../utils')
 const { transactionSpec, payableSpec } = require('../../utils/transactionSpec')
 
 class Transaction {
-  async getAll() {
+  async getAll(companyId) {
     try {
-      const response = await TransactionModel.findAll({ raw: true })
+      const response = await PayableModel.findAll({
+        include:[
+          { model: TransactionModel, where: { companyId } }
+        ]
+      })
       return defaultResponse(response)
     } catch (error) {
       return errorResponse(error)
     }
   }
   
-  async create(transactionData) {
+  async create(transactionData, companyId) {
     const transaction = await database.transaction()
     try {
       const transactionResponse = await TransactionModel.create(
-        transactionSpec(transactionData), { transaction }
+        transactionSpec({...transactionData, companyId}), { transaction }
       )
       
-      await PayableModel.create(payableSpec(transactionResponse), { transaction })
+      await PayableModel.create(
+        payableSpec(transactionResponse), { transaction }
+      )
+
       await transaction.commit()
       return defaultResponse(transactionResponse, 201)
     } catch (error) {
       await transaction.rollback()
       return errorResponse(error, 422)
-    }
-  }
-
-  async getByid(id) {
-    try {
-      const response = await TransactionModel.findByPk(id)
-      return defaultResponse(response)
-    } catch (error) {
-      return errorResponse(error)
     }
   }
 }
